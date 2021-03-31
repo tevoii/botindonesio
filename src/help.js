38a6cc93a1fcfda529966f04c666165b711c419f
tevoii <stevan.akb@gmail.com>
2021-03-31 13:56:27 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,59 @@
+const help = (prefix) => {
+	return `
+	
+☆┌─┐  ─┐☆
+　│▒│ /▒/
+　│▒│/▒/
+　│▒ /▒/─┬─┐◯
+　│▒│▒|▒│▒│
+┌┴─┴─┐-┘─┘
+│▒┌──┘▒▒▒│◯
+└┐▒▒▒▒▒▒┌┘
+◯└┐▒▒▒▒┌
+
+
+	> *fazer figurinha* <
+ *${prefix}sticker*  
+ *${prefix}toimg*
+ *${prefix}tsticker*
+
+
+> *Meme* <
+
+ *${prefix}meme*
+
+> *outros comandos * <
+
+ *${prefix}tp*
+ *${prefix}ep*
+ *${prefix}tahta*
+ *${prefix}gtts*
+ *${prefix}loli*
+ *${prefix}nsfwloli*
+ *${prefix}url2img*
+ *${prefix}simi*
+ *${prefix}ocr*
+ *${prefix}wait*
+ *${prefix}setprefix*
+
+> *Grupo* <
+
+ *${prefix}add*
+ *${prefix}kick*
+ *${prefix}promote*
+ *${prefix}demote*
+ *${prefix}linkgroup*
+ *${prefix}leave*
+ *${prefix}tagall*
+ *${prefix}simih*
+┈┈┈╱▔▔▔▔▔▔╲┈╭━━━╮┈┈
+┈┈▕┈╭━╮╭━╮┈▏┃ BOT ┃┈┈
+┈┈▕┈┃╭╯╰╮┃┈▏╰┳━━╯┈┈
+┈┈▕┈╰╯╭╮╰╯┈▏┈┃┈┈┈┈┈
+┈┈▕┈┈┈┃┃┈┈┈▏━╯┈┈┈┈┈
+┈┈▕┈┈┈╰╯┈┈┈▏┈┈┈┈┈┈┈
+┈┈▕╱╲╱╲╱╲╱╲▏┈┈┈┈┈┈┈
+`
+}
+
+exports.help = help
